36e979fb865fedc5c80f81f20edf02004e674f78
Seraphim McGann <seraphim.mcgann@yale.edu>
2025-05-12 16:54:11 -0400

    Incrementing to 3.0.1

--- src/globus/utils.ts
+++ src/globus/utils.ts
@@ -1,6 +1,6 @@
 export const CLIENT_INFO = {
   product: "@globus/static-search-portal",
   // x-release-please-start-version
-  version: "3.0.0",
+  version: "3.0.1",
   // x-release-please-end
 };
